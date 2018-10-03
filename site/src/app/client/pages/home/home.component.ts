f36eaa9b271f2ed22d4529317b2bfee58b75899f
Anderson Nunes <anderson.nunes17@etec.sp.gov.br>
2018-10-03 15:27:44 -0300

    Criação de Rotas Login

--- site/src/app/client/pages/home/home.component.ts
+++ site/src/app/client/pages/home/home.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'client-page-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class ClientHomeComponent implements OnInit {
+  
+  id: string;
+  
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    this.id = this.route.snapshot.params['id'];
+   }
 
   ngOnInit() {
   }
