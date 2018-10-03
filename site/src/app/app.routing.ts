5d921e8d25fbb74633ef91947c768a606262958f
Anderson Nunes <anderson.nunes17@etec.sp.gov.br>
2018-10-03 15:08:54 -0300

    Login Tela

--- site/src/app/app.routing.ts
+++ site/src/app/app.routing.ts
@@ -1,11 +1,14 @@
 import { ClientHomeComponent } from './client/pages/home/home.component';
 import { PainelHomeComponent } from './painel/pages/home/home.component';
 import {Routes, RouterModule} from '@angular/router';
-import {ModuleWithProviders} from '@angular/core';
+import { ModuleWithProviders, Component } from '@angular/core';
+import { UserLoginComponent } from './user/login/login.component';
 
 const app_routes:Routes=[
     {path: '', component: ClientHomeComponent},
     {path: 'painel', component: PainelHomeComponent},
+    {path: 'user/login', component: UserLoginComponent}
+    {path: 'user/:id', component: UserLoginComponent}
 ];
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(app_routes);
+export const routing: ModuleWithProviders = RouterModule.forRoot(app_routes); 
