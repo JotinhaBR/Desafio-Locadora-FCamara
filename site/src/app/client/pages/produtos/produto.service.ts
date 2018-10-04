01ed1e32ed9abd55073a3d9500339189e1370ead
Anderson Nunes <anderson.nunes17@etec.sp.gov.br>
2018-10-04 14:13:11 -0300

    Busca Filme

---
+++ site/src/app/client/pages/produtos/produto.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class FilmeService {
+  constructor(private http: HttpClient) {}
+
+  createFilme(json): Observable<any> {
+    return this.http.post('http://localhost:3000/produto/create', json);
+  }
+
+  buscarFilme(json): Observable<any> {
+    return this.http.get('http://localhost:3000/produto/find', json);
+  }
+
+  deleteFilme(json): Observable<any> {
+   return this.http.delete('http://localhost:3000/produto/deletar', json);
+  }
+}
